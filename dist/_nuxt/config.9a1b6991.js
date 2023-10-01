946107c0be1c1080cb37a4ba1adc1154c7d04d23
falana sheriff <falanasheriffdeen@gmail.com>
2023-10-01 21:44:25 +0100

    deploying

--- dist/_nuxt/config.9a1b6991.js
+++
@@ -1,1 +0,0 @@
-import{a3 as p,a4 as o,V as i}from"./entry.25559c4a.js";const a={nuxt:{}},t=p(a);function s(){const n=i();return n._appConfig||(n._appConfig=o(t)),n._appConfig}export{s as u};
--- dist/_nuxt/hang5.b1a0ca06.js
+++
@@ -1,1 +0,0 @@
-import"./entry.25559c4a.js";const i=""+globalThis.__publicAssetsURL("img/hang5.png");export{i as _};
